6abe7a93dccd35208db0fc98de0e4d458c9b24bf
nox <dark.tempus@gmail.com>
2023-03-13 21:26:37 -0500

    menu emergente con js

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+/* const $ = (selector) =>document.querySelector(selector);
+
+const menuEmail = $('.navbar-email');
+const desktopMenu = $('.desktop-menu'); */
+
+
+const menuEmail=document.querySelector('.navbar-email');
+const desktopMenu= document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+
+    desktopMenu.classList.toggle('inactive');
+
+
+}
+
+ 
