a5c64ccd888fec4f00a8c063285610748ec2cc03
nox <dark.tempus@gmail.com>
2023-03-14 00:26:21 -0500

    hide menus and cart js

--- src/js/main.js
+++ src/js/main.js
@@ -4,27 +4,39 @@ const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu'); */
 
 
-const menuEmail=document.querySelector('.navbar-email');
-const desktopMenu= document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
 
-const burguerMenu= document.querySelector('.menu');
-const mobileMenu= document.querySelector('.mobile-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-
-menuEmail.addEventListener('click',toggleDesktopMenu);
-burguerMenu.addEventListener('click',toggleMobileMenu);
-
+const cart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
 
-function toggleDesktopMenu(){
+cart.addEventListener('click', toggleCarritoAside);
+
+
+
+
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
- 
-function toggleMobileMenu(){
-mobileMenu.classList.toggle('inactive');
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
- 
+
+function toggleCarritoAside() {
+    desktopMenu.classList.add('inactive')
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+}
 
 
 
@@ -33,4 +45,4 @@ mobileMenu.classList.toggle('inactive');
 
 
 
- 
+
