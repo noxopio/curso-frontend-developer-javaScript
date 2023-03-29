516ffe76306ca5f6001b3505a56b5e5d8df8c143
nox <dark.tempus@gmail.com>
2023-03-29 10:21:06 -0500

    .gitignore

--- src/js/main.js
+++ src/js/main.js
@@ -186,3 +186,25 @@ console.log('ready')
  
 
 //-----------------------fin de  funcion--------------------------------------
+
+//---------------------------------------------------------
+function Fibonacci (n) {
+    let siguiente = 2;
+    let fibonacci = [0, 1];
+
+    while ( siguiente < Number(n) ) {
+        
+        let suma = Number(fibonacci[ siguiente - 2 ] + fibonacci[ siguiente - 1 ]);
+        fibonacci.push(suma);
+        siguiente++;
+    }
+    
+    for (let i = 0; i < fibonacci.length; i++) {
+        const element = fibonacci[i];
+        console.log(`Iteración número ${i} : ${element}`);
+    }
+}
+
+const cantidad =12 //prompt('Cantidad de números');
+Fibonacci(cantidad);
+//-----------------------------------------------
